9e3501a7bdb4e786e7c73cf1bde66fa9a0df04b1
Halvard Sutterud <halvard.sutterud@gmail.com>
2018-06-06 18:05:51 +0200

    fix speed thing

--- exana/tracking/speed.py
+++ exana/tracking/speed.py
@@ -4,35 +4,40 @@ import neo
 import elephant as ep
 # import exana.tracking as tr
 
-def speed_correlation(speed,t,sptr, stddev = 90*pq.ms):
+def speed_correlation(speed,t,sptr, stddev = 250*pq.ms, min_speed =
+        0.0*pq.m*pq.Hz, max_speed_percentile = 100, return_data = False):
     """
-    correlates instantaneous spike rate and rat velocity, using a method
+    Correlates instantaneous spike rate and rat velocity, using a method
     described in [1]
+
+    Parameters:
+    -----------
+    speed, t : 
+
+    sptr : 
+
+    Returns
+    -------
+    
+    out : correlation, (pearson, inst_speed)
 
     [1]: 
     """
 
-    import matplotlib.pyplot as plt
-
-    min_speed = 0.0*pq.m*pq.Hz
-    max_speed = np.percentile(speed, 100 )
-    mask = np.logical_and(speed < max_speed,speed > min_speed)
+    from scipy.interpolate import interp1d
+    max_speed = np.percentile(speed, max_speed_percentile)
+    mask = np.logical_and(speed < max_speed, speed > min_speed)
 
     t_ = t[:speed.size][mask]
     speed = speed[mask]
 
     dt = np.average(np.diff(t_))
 
-    t_binsize = dt
-    stddev = t_binsize / dt
-
-    corr = np.corrcoef(speed[:-1], instant_spike_rate)
-
-    interp_speed = interp1d(t,speed,  bounds_error=False, fill_value = (speed[0],speed[-1]))
+    interp_speed = interp1d(t_,speed,  bounds_error=False, fill_value = (speed[0], speed[-1]))
     binsize = np.mean(np.diff(t)).rescale('s') * 2
 
-    rate = ep.statistics.instantaneous_rate(sptr, binsize,t_start = t[0], t_stop = t[-1], 
-                            kernel = ep.kernels.GaussianKernel(sigma=250*pq.ms))
+    rate = ep.statistics.instantaneous_rate(sptr, binsize, t_start = t[0], t_stop = t[-1], 
+                            kernel = ep.kernels.GaussianKernel(sigma=stddev))
 
     mask = np.logical_and(rate.times <= t[-1], rate.times >= t[0])
     inst_speed = interp_speed(rate.times[mask].rescale('1/Hz'))
